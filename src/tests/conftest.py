7bd643e04ab4ff6de49db3be8081aa2e7ebf7b49
lauro <lauro.gama@gmail.com>
2021-12-28 19:45:11 -0400

    testing with pytest-cov
    feat: add pytest-cov

---
+++ src/tests/conftest.py
@@ -0,0 +1,10 @@
+import pytest
+
+from api.main import app
+from starlette.testclient import TestClient
+
+
+@pytest.fixture(scope="module")
+def test_app():
+    client = TestClient(app)
+    yield client  # testing happens here
--- src/tests/test_api.py
+++ src/tests/test_api.py
@@ -1,13 +1,7 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from api.main import app
-
-client = TestClient(app)
-
-
-def test_main():
-    response = client.get("/items/3")
+def test_main(test_app):
+    response = test_app.get("/items/3")
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {"item_id": 3}
